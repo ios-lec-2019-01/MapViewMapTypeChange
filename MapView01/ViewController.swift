fe9e1305dc63e793f964a69ed4de13ed343ceb76
Jonghyun Kim <jhkim@dit.ac.kr>
2019-09-16 19:43:23 +0900

    1st commit

--- MapView01/ViewController.swift
+++ MapView01/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // standard, satellite, hybird
+        mapView.mapType = MKMapType.satellite
+        
+        // DIT  35.166060, 129.072522
+        let location = CLLocationCoordinate2D(latitude:
+            35.166060, longitude: 129.072522)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+//        let region = MKCoordinateRegion(center: location, latitudinalMeters: 300, longitudinalMeters: 300)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "동의과학대학교"
+        annotation.subtitle = "DIT"
+        mapView.addAnnotation(annotation)
     }
 
-
+    @IBAction func standardMapTypeAction(_ sender: Any) {
+        mapView.mapType = MKMapType.standard
+    }
+    
+    @IBAction func satelliteMapTypeAction(_ sender: Any) {
+        mapView.mapType = MKMapType.satellite
+    }
+    @IBAction func hybridMapTypeAction(_ sender: Any) {
+        mapView.mapType = MKMapType.hybrid
+    }
 }
 
